74cb239bd17465174823e5d07cdbafb6852a5042
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2022-12-27 16:16:30 +0000

    c

--- TrabalhoSD/src/Cliente.java
+++ TrabalhoSD/src/Cliente.java
@@ -1,8 +1,11 @@
 public class Cliente {
     private String nome;
     private String password;
-    private int cordX;
-    private int cordy;
+
+    public Cliente(String nome, String password) {
+        this.nome = nome;
+        this.password = password;
+    }
 
     public String getNome() {
         return nome;
@@ -18,21 +21,5 @@ public class Cliente {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getCordX() {
-        return cordX;
-    }
-
-    public void setCordX(int cordX) {
-        this.cordX = cordX;
-    }
-
-    public int getCordy() {
-        return cordy;
-    }
-
-    public void setCordy(int cordy) {
-        this.cordy = cordy;
     }
 }
--- TrabalhoSD/src/Server.java
+++ TrabalhoSD/src/Server.java
@@ -2,13 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
-    List<List<String>> mapa= new ArrayList<>();
+    List<List<Trotinete>> mapa= new ArrayList<>();
+
+    List<Trotinete> trotinetes = new ArrayList<>();
+
+    final Integer distancia = 2;
+    final Integer tamanho = 20;
 
     public void criaMapa(){
-        for(int i = 0; i<20; i++){
-            for(int j = 0; j<20; j++){
+        for(int i = 0; i<this.tamanho; i++){
+            mapa.add(new ArrayList<>());
+            for(int j = 0; j<this.tamanho; j++){
                 mapa.get(i).add(null);
             }
         }
+
+        for (Trotinete t : this.trotinetes) {
+            int x = t.getCorX();
+            int y = t.getCorY();
+            mapa.get(y).add(x, t);
+        }
     }
+
+    public List<List<Integer>> trotinetes_livres(int x,int y){}
+
+    public List<List<Integer>> recompensas(){}
+
+    public void reserva_trotinete(int x ,int y){}
 }
