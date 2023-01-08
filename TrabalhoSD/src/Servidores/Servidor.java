5b6e9c34c4743744dd266ccd317a2b2af789b8db
0HenriqueFernandes0 <a95323@alunos.uminho.pt>
2023-01-08 17:16:45 +0000

    c

--- TrabalhoSD/src/Servidores/Servidor.java
+++ TrabalhoSD/src/Servidores/Servidor.java
@@ -1,5 +1,8 @@
 package Servidores;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 
 public class Servidor {
@@ -16,9 +19,53 @@ public class Servidor {
         }
     }
 
-
-    public void start() {
-
+    public void clientes (Socket s) {
+        Thread espera_notificacao = null;
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            while (true){
+                int i = in.read();
+                if(i==1){
+                    int x = in.read();
+                    int y = in.read();
+                    this.aplication.trotinetes_livres(x,y);
+                }else if(1==2){
+                } else if (1==3) {
+                    int x = in.read();
+                    int y = in.read();
+                    this.aplication.reserva_trotinete(x,y);
+                } else if (i==4) {
+                    if (espera_notificacao == null){
+                        espera_notificacao = new Thread(()->{espera_notificacoes(s);});
+                        espera_notificacao.start();}
+                } else if (i==5) {
+                    if (espera_notificacao != null) {
+                        espera_notificacao.interrupt();
+                        espera_notificacao = null;}
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private void espera_notificacoes(Socket s) {
+    }
+
+    private void recompensas() {
+    }
+
+    public void start() throws IOException {
+        Thread Recompesas = new Thread(()->{recompensas();});
+        Recompesas.start();
+        ServerSocket ss = new ServerSocket(1100);
+        while (true){
+            Socket s = ss.accept();
+            Thread cliente = new Thread(() -> {clientes(s);});
+            cliente.start();
+        }
+    }
+
 }
 
