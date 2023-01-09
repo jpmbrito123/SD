251876172642d70e7142e7e38221aa08340ac077
andregoncalves02 <73440927+andregoncalves02@users.noreply.github.com>
2023-01-09 17:32:46 +0000

    Update Text2UI.java

--- TrabalhoSD/src/UI/Text2UI.java
+++ TrabalhoSD/src/UI/Text2UI.java
@@ -3,22 +3,23 @@ package UI;
 import Clientes.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.io.StreamCorruptedException;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Text2UI {
-    
-    private String senha;
+
     // Menus da aplicação
     private Menu menu;
 
     // Scanner para leitura
     private Scanner scin;
+
+    private String senha;
     
     private Demultiplexer multi;
+
+    private Thread thread = new Thread(this::trataPedirNotificacoes);
 
     /**
      * Construtor.
@@ -50,29 +51,64 @@ public class Text2UI {
     }
 
     private void trataDesativarNotificacoes() {
+        this.thread.start();
+        try {
+            multi.send(8,"Desativar Notificacao".getBytes());
+            byte[] reply = multi.receive(8);
+            int error = Integer.parseInt(new String(reply));
+            if(error==0){
+                this.thread.join();
+                System.out.println("Notificaçoes desativadas");
+            }
+            else System.out.println("\033[0;31m" + ": Falha ao desativar notificacoes" + "\n\n\033[0m");
+
+        }
+        catch (NullPointerException | IOException | InterruptedException e){
+            System.out.print(e.getMessage() + "\n\n");
+
+        }
     }
+    
 
     private void trataPedirNotificacoes() {
+        this.thread.start();
+        try {
+            multi.send(7,"Ativar Notificacao".getBytes());
+            byte[] reply = multi.receive(7);
+            int error = Integer.parseInt(new String(reply));
+            if(error==0){
+                System.out.println("Notificaçoes ativadas");
+            }
+            else this.thread.join();
+
+        }
+        catch (NullPointerException | IOException | InterruptedException e){
+            System.out.print(e.getMessage() + "\n\n");
+
+        }
     }
 
-   private void trataEstacionar() throws InterruptedException {
+    private void trataEstacionar() throws InterruptedException {
         Thread t = new Thread(() -> {
             try{
                 System.out.println("Insira as suas coordenadas: ");
                 String coordenadas = scin.nextLine();
                 multi.send(6,(coordenadas + " " + this.senha).getBytes());
-                byte[] reply = multi.receive(5);
+                byte[] reply = multi.receive(6);
                 int error = Integer.parseInt(new String(reply));
                 System.out.println("\n\n");
                 if(error==0){
-                    byte[] reply1 = multi.receive(5);
+                    byte[] reply1 = multi.receive(6);
                     String preco = new String(reply1);
-                    System.out.println("O valor da viagem é: " + preco);
+                    System.out.println("O valor da viagem é: " + preco + "$");
                 }
+                else
+                    System.out.println("\033[0;31m" + ": Falha ao estacionar a trotinete" + "\n\n\033[0m");
 
 
             }
             catch(NullPointerException | IOException | InterruptedException e){
+                System.out.print(e.getMessage() + "\n\n");
             }
 
         });
@@ -130,6 +166,7 @@ public class Text2UI {
         t.start();
         t.join();
     }
+
     /**
      * Executa o menu principal e invoca o método correspondente à opção seleccionada.
      */
